3193902dcb2d3a04b5c09ff935d75870f55dec63
Семён Зубань <zuban.semyon@gmail.com>
2022-12-27 17:33:27 +1000

    Code according to PEP8

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def attack(char_name, char_class):
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(-3, -1)}')
-    return ('Невозможно определить урон.')
+    return (f'{char_name} не применил атаку.')
 
 
 def defence(char_name, char_class):
@@ -21,7 +21,7 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-    return ('Невозможно оценить защиту.')
+    return (f'{char_name} не применил защиту.')
 
 
 def special(char_name, char_class):
